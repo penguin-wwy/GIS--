7dac0255eb7d2b59a8d3eed35c97097dc35c2cba
penguin-wwy <wwy950210@163.com>
2015-12-25 23:43:37 +0800

    Spead_lots_of_times
    已更改版本

---
+++ Spead_lots_of_times/Spead_lots_of_times/Form1.Designer.cs
@@ -0,0 +1,290 @@
+﻿namespace Spread_lots_of_times
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.文件ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.New_MenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.axToolbarControl1 = new AxESRI.ArcGIS.Controls.AxToolbarControl();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.splitContainer2 = new System.Windows.Forms.SplitContainer();
+            this.axTOCControl1 = new AxESRI.ArcGIS.Controls.AxTOCControl();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPageMap = new System.Windows.Forms.TabPage();
+            this.axMapControl1 = new AxESRI.ArcGIS.Controls.AxMapControl();
+            this.tabPageLayout = new System.Windows.Forms.TabPage();
+            this.axPageLayoutControl1 = new AxESRI.ArcGIS.Controls.AxPageLayoutControl();
+            this.axLicenseControl1 = new AxESRI.ArcGIS.Controls.AxLicenseControl();
+            this.Open_MenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.Add_Data_MenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.Exit_MenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.axToolbarControl1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).BeginInit();
+            this.splitContainer2.Panel1.SuspendLayout();
+            this.splitContainer2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.axTOCControl1)).BeginInit();
+            this.tabControl1.SuspendLayout();
+            this.tabPageMap.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.axMapControl1)).BeginInit();
+            this.tabPageLayout.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.axPageLayoutControl1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axLicenseControl1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.文件ToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(475, 25);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // 文件ToolStripMenuItem
+            // 
+            this.文件ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.New_MenuItem,
+            this.Open_MenuItem,
+            this.Add_Data_MenuItem,
+            this.Exit_MenuItem});
+            this.文件ToolStripMenuItem.Name = "文件ToolStripMenuItem";
+            this.文件ToolStripMenuItem.Size = new System.Drawing.Size(44, 21);
+            this.文件ToolStripMenuItem.Text = "文件";
+            // 
+            // New_MenuItem
+            // 
+            this.New_MenuItem.Name = "New_MenuItem";
+            this.New_MenuItem.Size = new System.Drawing.Size(179, 22);
+            this.New_MenuItem.Text = "新建&New";
+            this.New_MenuItem.Click += new System.EventHandler(this.New_MenuItem_Click);
+            // 
+            // axToolbarControl1
+            // 
+            this.axToolbarControl1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.axToolbarControl1.Location = new System.Drawing.Point(0, 25);
+            this.axToolbarControl1.Name = "axToolbarControl1";
+            this.axToolbarControl1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axToolbarControl1.OcxState")));
+            this.axToolbarControl1.Size = new System.Drawing.Size(475, 28);
+            this.axToolbarControl1.TabIndex = 1;
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Location = new System.Drawing.Point(0, 350);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(475, 22);
+            this.statusStrip1.TabIndex = 2;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 53);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.splitContainer2);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.tabControl1);
+            this.splitContainer1.Size = new System.Drawing.Size(475, 297);
+            this.splitContainer1.SplitterDistance = 172;
+            this.splitContainer1.TabIndex = 3;
+            // 
+            // splitContainer2
+            // 
+            this.splitContainer2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer2.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer2.Name = "splitContainer2";
+            this.splitContainer2.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer2.Panel1
+            // 
+            this.splitContainer2.Panel1.Controls.Add(this.axTOCControl1);
+            this.splitContainer2.Size = new System.Drawing.Size(172, 297);
+            this.splitContainer2.SplitterDistance = 186;
+            this.splitContainer2.TabIndex = 0;
+            // 
+            // axTOCControl1
+            // 
+            this.axTOCControl1.Location = new System.Drawing.Point(3, 0);
+            this.axTOCControl1.Name = "axTOCControl1";
+            this.axTOCControl1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axTOCControl1.OcxState")));
+            this.axTOCControl1.Size = new System.Drawing.Size(265, 265);
+            this.axTOCControl1.TabIndex = 1;
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Alignment = System.Windows.Forms.TabAlignment.Bottom;
+            this.tabControl1.Controls.Add(this.tabPageMap);
+            this.tabControl1.Controls.Add(this.tabPageLayout);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(299, 297);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPageMap
+            // 
+            this.tabPageMap.Controls.Add(this.axMapControl1);
+            this.tabPageMap.Location = new System.Drawing.Point(4, 4);
+            this.tabPageMap.Name = "tabPageMap";
+            this.tabPageMap.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPageMap.Size = new System.Drawing.Size(291, 271);
+            this.tabPageMap.TabIndex = 0;
+            this.tabPageMap.Text = "地图";
+            this.tabPageMap.UseVisualStyleBackColor = true;
+            // 
+            // axMapControl1
+            // 
+            this.axMapControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.axMapControl1.Location = new System.Drawing.Point(3, 3);
+            this.axMapControl1.Name = "axMapControl1";
+            this.axMapControl1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axMapControl1.OcxState")));
+            this.axMapControl1.Size = new System.Drawing.Size(285, 265);
+            this.axMapControl1.TabIndex = 0;
+            // 
+            // tabPageLayout
+            // 
+            this.tabPageLayout.Controls.Add(this.axPageLayoutControl1);
+            this.tabPageLayout.Location = new System.Drawing.Point(4, 4);
+            this.tabPageLayout.Name = "tabPageLayout";
+            this.tabPageLayout.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPageLayout.Size = new System.Drawing.Size(291, 271);
+            this.tabPageLayout.TabIndex = 1;
+            this.tabPageLayout.Text = "制版";
+            this.tabPageLayout.UseVisualStyleBackColor = true;
+            // 
+            // axPageLayoutControl1
+            // 
+            this.axPageLayoutControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.axPageLayoutControl1.Location = new System.Drawing.Point(3, 3);
+            this.axPageLayoutControl1.Name = "axPageLayoutControl1";
+            this.axPageLayoutControl1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axPageLayoutControl1.OcxState")));
+            this.axPageLayoutControl1.Size = new System.Drawing.Size(285, 265);
+            this.axPageLayoutControl1.TabIndex = 0;
+            // 
+            // axLicenseControl1
+            // 
+            this.axLicenseControl1.Enabled = true;
+            this.axLicenseControl1.Location = new System.Drawing.Point(443, 350);
+            this.axLicenseControl1.Name = "axLicenseControl1";
+            this.axLicenseControl1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axLicenseControl1.OcxState")));
+            this.axLicenseControl1.Size = new System.Drawing.Size(32, 32);
+            this.axLicenseControl1.TabIndex = 1;
+            // 
+            // Open_MenuItem
+            // 
+            this.Open_MenuItem.Name = "Open_MenuItem";
+            this.Open_MenuItem.Size = new System.Drawing.Size(179, 22);
+            this.Open_MenuItem.Text = "打开&Open";
+            this.Open_MenuItem.Click += new System.EventHandler(this.Open_MenuItem_Click);
+            // 
+            // Add_Data_MenuItem
+            // 
+            this.Add_Data_MenuItem.Name = "Add_Data_MenuItem";
+            this.Add_Data_MenuItem.Size = new System.Drawing.Size(179, 22);
+            this.Add_Data_MenuItem.Text = "添加数据Add &Data";
+            this.Add_Data_MenuItem.Click += new System.EventHandler(this.Add_Data_MenuItem_Click);
+            // 
+            // Exit_MenuItem
+            // 
+            this.Exit_MenuItem.Name = "Exit_MenuItem";
+            this.Exit_MenuItem.Size = new System.Drawing.Size(179, 22);
+            this.Exit_MenuItem.Text = "退出&Exit";
+            this.Exit_MenuItem.Click += new System.EventHandler(this.Exit_MenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(475, 372);
+            this.Controls.Add(this.axLicenseControl1);
+            this.Controls.Add(this.splitContainer1);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.axToolbarControl1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "提莫队长";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.axToolbarControl1)).EndInit();
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.splitContainer2.Panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).EndInit();
+            this.splitContainer2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.axTOCControl1)).EndInit();
+            this.tabControl1.ResumeLayout(false);
+            this.tabPageMap.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.axMapControl1)).EndInit();
+            this.tabPageLayout.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.axPageLayoutControl1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axLicenseControl1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private AxESRI.ArcGIS.Controls.AxToolbarControl axToolbarControl1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.SplitContainer splitContainer2;
+        private AxESRI.ArcGIS.Controls.AxTOCControl axTOCControl1;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPageMap;
+        private AxESRI.ArcGIS.Controls.AxMapControl axMapControl1;
+        private System.Windows.Forms.TabPage tabPageLayout;
+        private AxESRI.ArcGIS.Controls.AxPageLayoutControl axPageLayoutControl1;
+        private AxESRI.ArcGIS.Controls.AxLicenseControl axLicenseControl1;
+        private System.Windows.Forms.ToolStripMenuItem 文件ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem New_MenuItem;
+        private System.Windows.Forms.ToolStripMenuItem Open_MenuItem;
+        private System.Windows.Forms.ToolStripMenuItem Add_Data_MenuItem;
+        private System.Windows.Forms.ToolStripMenuItem Exit_MenuItem;
+    }
+}
+
